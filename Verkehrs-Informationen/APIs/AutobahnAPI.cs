a9b1a36d2b857341481ec83a14405ca038dde9af
Mehdi Boudjoudi <mehboudj@uni-bremen.de>
2026-01-30 12:40:33 +0100

    api endpunkt call für warnungen

--- Verkehrs-Informationen/APIs/AutobahnAPI.cs
+++ Verkehrs-Informationen/APIs/AutobahnAPI.cs
@@ -1,15 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
-namespace Verkehrs_Informationen.APIs
+namespace Verkehrs_Informationen.APIs;
+
+public class AutobahnService
 {
-    internal class AutobahnAPI
+    private static readonly HttpClient _httpClient = new HttpClient
     {
+        BaseAddress = new Uri("https://autobahn.api.bund.dev/roadmaps/")
+    };
 
-
-
+    public async Task<List<WarningItem>?> GetWarningsAsync(string roadId)
+    {
+        try
+        {
+            var response = await _httpClient.GetFromJsonAsync<WarningResponse>($"{roadId}/services/warning");
+            return response?.Warning ?? new List<WarningItem>();
+        }
+        catch (Exception ex)        {
+            Console.WriteLine($"Fehler beim Abruf: {ex.Message}");
+            return null;
+        }
     }
 }
---
+++ Verkehrs-Informationen/Models/APIModels/WarningAPIModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Verkehrs_Informationen.Models.APIModels
+{
+    public class WarningAPIModel
+    {
+        public List<WarningItemAPIModel> Warning { get; set; }
+    }
+
+    public class WarningItemAPIModel
+    {
+        public string Title { get; set; }
+        public string Subtitle { get; set; }
+        public string Description { get; set; }
+    }
+}
